31c42350f301f9e4ebbb704fa61052fa2813aa5e
Konstantinov <egaega802@gmail.com>
2023-05-28 23:52:15 +0300

    e.konstantinov
    db update

--- migrations/sqls/20230518110705-database-initial-down.sql
+++ migrations/sqls/20230518110705-database-initial-down.sql
@@ -1,2 +1,4 @@
+DROP TABLE "answers";
+DROP TABLE "tasks";
 DROP TABLE "users";
 DROP TABLE "roles";
--- migrations/sqls/20230518110705-database-initial-up.sql
+++ migrations/sqls/20230518110705-database-initial-up.sql
@@ -32,3 +32,33 @@ COMMENT ON COLUMN "users"."password" IS 'Пароль пользователя';
 COMMENT ON COLUMN "users"."role_id" IS 'Идентификатор роли пользователя';
 COMMENT ON COLUMN "users"."createdAt" IS 'Время создания записи';
 COMMENT ON COLUMN "users"."updatedAt" IS 'Время обновления записи';
+
+-- Таблица заданий --
+CREATE TABLE IF NOT EXISTS "tasks" (
+    "id"                UUID    PRIMARY KEY     DEFAULT gen_random_uuid (),
+    "value"             VARCHAR(999)     DEFAULT 'Условие отсутствует',  
+    "createdAt"         TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"         TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE "tasks" IS 'Таблица пользователей';
+COMMENT ON COLUMN "tasks"."id" IS 'Идентификатор задачи';
+COMMENT ON COLUMN "tasks"."value" IS 'Текст задания';
+COMMENT ON COLUMN "tasks"."createdAt" IS 'Время создания записи';
+COMMENT ON COLUMN "tasks"."updatedAt" IS 'Время обновления записи';
+
+-- Таблица ответа --
+CREATE TABLE IF NOT EXISTS "answers" (
+    "id"                UUID    PRIMARY KEY     DEFAULT gen_random_uuid (),
+    "value"             VARCHAR(999)     DEFAULT 'Ответ отсутствует',  
+    "task_id"           UUID    REFERENCES "tasks"("id"),
+    "user_id"           UUID    REFERENCES "users"("id"),
+    "createdAt"         TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"         TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE "answers" IS 'Таблица пользователей';
+COMMENT ON COLUMN "answers"."id" IS 'Идентификатор задачи';
+COMMENT ON COLUMN "answers"."value" IS 'Ответ на задания';
+COMMENT ON COLUMN "answers"."task_id" IS 'Ссылка на задачу';
+COMMENT ON COLUMN "answers"."user_id" IS 'Ссылка на пользователя';
+COMMENT ON COLUMN "answers"."createdAt" IS 'Время создания записи';
+COMMENT ON COLUMN "answers"."updatedAt" IS 'Время обновления записи';
